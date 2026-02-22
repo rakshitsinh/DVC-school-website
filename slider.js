437972cea0a304da732e2359aa92ea141e7ea820
Rakshit Sinha <158760628+rakshitsinh@users.noreply.github.com>
2026-02-22 12:40:58 +0530

    Delete slider.js

--- slider.js
+++
@@ -1,7 +0,0 @@
-let images = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image6.jpg", "images/image7.jpg", "images/image8.jpg", "images/image9.jpg", "images/image11.jpg"];
-let index = 0;
-
-setInterval(function() {
-    index = (index + 1) % images.length;
-    document.getElementById("slide").src = images[index];
-}, 3000);
